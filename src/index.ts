3888e43cf8fe8b04ec2c5ab42045753d476617fc
biocity93 <90408597+biocity93@users.noreply.github.com>
2022-08-13 11:18:33 -0400

    query writing to db

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("hello rgre");
+import { PrismaClient } from '@prisma/client'
+
+// use `prisma` in your application to read and write data in your DB
+
+const main =async () => {
+    const prisma = new PrismaClient()
+    const newUser = await prisma.user.create({
+        data:{
+            name: '',
+            email: '',
+        },
+    })
+    const users = await prisma.user.findMany()
+    
+}
+
+console.log("hello test");
